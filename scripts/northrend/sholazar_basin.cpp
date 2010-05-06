4050b6ca5411bc8038ca283f6b2e40b9fdd40378
GMHAZE <iceis2@hotmail.com>
2010-05-06 13:59:57 -0400

    Q The Taste Test (Sholazar Basin)

--- scripts/northrend/sholazar_basin.cpp
+++ scripts/northrend/sholazar_basin.cpp
@@ -22,6 +22,7 @@ SDCategory: Sholazar Basin
 EndScriptData */
 
 /* ContentData
+mob_rjr_target
 npc_vekjik
 EndContentData */
 
@@ -29,11 +30,89 @@ EndContentData */
 #include "escort_ai.h"
 
 /*######
+## mob_rjr_target
+######*/
+
+enum rjr_target
+{
+    NPC_WILHELM             = 28054,
+    NPC_DROSTAN             = 28328,
+
+    SPELL_HIT_APPLE         = 51331,
+    SPELL_HIT_BIRD          = 51366,
+    SPELL_HIT_WILHELM       = 51332,
+    SPELL_BIRD_FALL         = 51369,
+    SPELL_APPLE_FALL        = 51371
+};
+
+int DrostanText[] = {-1999835,-1999833,-1999831,-1999830};
+int WilhelmText[] = {-1999836,-1999834,-1999832,-1999829};
+
+struct MANGOS_DLL_DECL mob_rjr_targetAI : public ScriptedAI
+{
+    mob_rjr_targetAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+
+    bool bResponce;
+    uint32 m_uiResponceTimer;
+
+    void Reset() 
+    {
+        bResponce = false;
+        m_uiResponceTimer = 2000;
+    }
+
+    void SpellHit(Unit* pCaster, const SpellEntry *pSpell)
+    {
+        if (!pCaster || pCaster->GetTypeId() != TYPEID_PLAYER || !pSpell || !m_creature->isAlive())
+            return;
+
+        switch (pSpell->Id)
+        {
+            case SPELL_HIT_APPLE:
+                if (Unit* pWilhelm = GetClosestCreatureWithEntry(m_creature,NPC_WILHELM,30.0f))
+                    DoScriptText(WilhelmText[3],pWilhelm,pCaster);
+                DoCast(m_creature,SPELL_APPLE_FALL,true);
+                ((Player*)pCaster)->KilledMonsterCredit(m_creature->GetEntry(),m_creature->GetGUID());
+                m_creature->ForcedDespawn();
+                break;
+            case SPELL_HIT_BIRD:
+                if (Unit* pDrostan = GetClosestCreatureWithEntry(m_creature,NPC_DROSTAN,30.0f))
+                    DoScriptText(DrostanText[3],pDrostan,m_creature);
+                DoCast(m_creature,SPELL_BIRD_FALL,true);
+                break;
+            case SPELL_HIT_WILHELM:
+                DoScriptText(WilhelmText[urand(0,2)],m_creature);
+                bResponce = true;
+                break;
+                
+            default: break;
+        }
+    }
+
+    void UpdateAI (uint32 const uiDiff)
+    {
+        if (bResponce)
+            if (Unit* pDrostan = GetClosestCreatureWithEntry(m_creature,NPC_DROSTAN,30.0f))
+                if (m_uiResponceTimer <= uiDiff)
+                {
+                    DoScriptText(DrostanText[urand(0,2)],pDrostan,m_creature);
+                    m_uiResponceTimer = 2000;
+                    bResponce = false;
+                }else m_uiResponceTimer -= uiDiff;
+    }
+};
+
+CreatureAI* GetAI_mob_rjr_target(Creature* pCreature)
+{
+    return new mob_rjr_targetAI(pCreature);
+}
+
+/*######
 ## npc_vekjik
 ######*/
 
-#define GOSSIP_VEKJIK_ITEM1 "Schamane Vekjik, ich habe mit den Großzungen gesprochen und sie wollen Frieden. Auf ihr Geheiß hin \303\274berbringe ich dieses Angebot."
-#define GOSSIP_VEKJIK_ITEM2 "Nein, nein... Ich hatte nie vor, unser Volk zu verraten. Ich habe mich nur selbst verteidigt. Es war alles ein Missverst\303\244ndnis."
+#define GOSSIP_VEKJIK_ITEM1 "Shaman Vekjik, I have spoken with the big-tongues and they desire peace. I have brought this offering on their behalf."
+#define GOSSIP_VEKJIK_ITEM2 "No no... I had no intentions of betraying your people. I was only defending myself. it was all a misunderstanding."
 
 enum
 {
@@ -169,21 +248,107 @@ bool QuestAccept_fortunate_misunderstandings(Player* pPlayer, Creature* pCreatur
     return true;
 }
 
+enum TasteTest
+{
+    SPELL_OFFER_JUNGLE_PUNCH    = 51962,
+
+    NPC_HEMET_NESINGWAY         = 27986,
+    NPC_HADRIUS_HARLOWE         = 28047,
+    NPC_TAMARA_WOBBLESPROCKET   = 28568
+};
+
+int32 HemetText[3] = {-1999791, -1999790, -1999789};
+int32 HadriusText[3] = {-1999788, -1999787, -1999786};
+int32 TamaraText[3] = {-1999785, -1999784, -1999783};
+
+struct MANGOS_DLL_DECL mob_taste_testAI : public ScriptedAI
+{
+    mob_taste_testAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+
+    bool bTasteing;
+    uint8 Phase;
+    uint32 m_uiEventTimer;
+    uint64 m_uiPlayersGUID;
+
+    void Reset() 
+    {
+        bTasteing = false;
+        Phase = 0;
+        m_uiPlayersGUID = 0;
+        m_uiEventTimer = 5000;
+    }
+
+    void UpdateAI (const uint32 uiDiff)
+    {
+        if (bTasteing)
+        {
+            if (m_uiEventTimer <= uiDiff)
+            {
+                switch(m_creature->GetEntry())
+                {
+                    case NPC_HEMET_NESINGWAY: DoScriptText(HemetText[Phase], m_creature); break;
+                    case NPC_HADRIUS_HARLOWE: DoScriptText(HadriusText[Phase], m_creature); break;
+                    case NPC_TAMARA_WOBBLESPROCKET: DoScriptText(TamaraText[Phase], m_creature); break;
+                    default: break;
+                }
+                if (Phase == 2)
+                {
+                    if (Player* pPlayer = (Player*)Unit::GetUnit((*m_creature),m_uiPlayersGUID))
+                        pPlayer->KilledMonsterCredit(m_creature->GetEntry(), m_creature->GetGUID());
+                    Reset();
+                    return;
+                }
+                ++Phase;
+                m_uiEventTimer = 5000;
+            } else m_uiEventTimer -= uiDiff;
+        }
+    }
+};
+
+bool EffectDummyCreature_mob_taste_test(Unit *pCaster, uint32 spellId, SpellEffectIndex effIndex, Creature *pCreatureTarget)
+{
+    if (spellId == SPELL_OFFER_JUNGLE_PUNCH && effIndex == EFFECT_INDEX_1 && pCaster->GetTypeId() == TYPEID_PLAYER && pCreatureTarget)
+    {
+        if (pCreatureTarget->AI())
+        {
+            ((mob_taste_testAI*)pCreatureTarget->AI())->bTasteing = true;
+            ((mob_taste_testAI*)pCreatureTarget->AI())->m_uiPlayersGUID = pCaster->GetGUID();
+        }
+    }
+    return true;
+}
+
+CreatureAI* GetAI_mob_taste_test(Creature* pCreature)
+{
+    return new mob_taste_testAI(pCreature);
+}
+
 void AddSC_sholazar_basin()
 {
     Script *newscript;
 
     newscript = new Script;
-    newscript->Name = "npc_vekjik";
-    newscript->pGossipHello = &GossipHello_npc_vekjik;
-    newscript->pGossipSelect = &GossipSelect_npc_vekjik;
+    newscript->Name = "mob_taste_test";
+    newscript->GetAI = &GetAI_mob_taste_test;
+    newscript->pEffectDummyCreature = &EffectDummyCreature_mob_taste_test;
     newscript->RegisterSelf();
-	
-	newscript = new Script;
+
+    newscript = new Script;
+    newscript->Name = "mob_rjr_target";
+    newscript->GetAI = &GetAI_mob_rjr_target;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
     newscript->Name = "npc_injured_oracle";
     newscript->GetAI = &GetAI_npc_injured_oracle;
     newscript->pQuestAccept = &QuestAccept_fortunate_misunderstandings;
     newscript->pGossipHello = &GossipHello_npc_injured_oracle;
     newscript->pGossipSelect = &GossipSelect_npc_injured_oracle;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_vekjik";
+    newscript->pGossipHello = &GossipHello_npc_vekjik;
+    newscript->pGossipSelect = &GossipSelect_npc_vekjik;
     newscript->RegisterSelf();
 }

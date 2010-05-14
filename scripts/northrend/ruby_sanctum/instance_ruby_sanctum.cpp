5a606f4867adc7f6a34c0a0af2abdbf07eb5be22
notagain <djastralxtc@msn.com>
2010-05-15 01:23:33 +1000

    ruby sanct work with phases still in slow dev till blizzlike fight is available

--- scripts/northrend/ruby_sanctum/instance_ruby_sanctum.cpp
+++ scripts/northrend/ruby_sanctum/instance_ruby_sanctum.cpp
@@ -24,86 +24,109 @@ EndScriptData */
 #include "precompiled.h"
 #include "ruby_sanctum.h"
 
-/* Ruby Sanctum encounters:
-0 - Halion
-*/
-
 struct MANGOS_DLL_DECL instance_ruby_sanctum : public ScriptedInstance
 {
-    instance_ruby_sanctum(Map* pMap) : ScriptedInstance(pMap) {Initialize();};
+    instance_ruby_sanctum(Map *pMap) : ScriptedInstance(pMap) {Initialize();}
 
     uint32 m_auiEncounter[MAX_ENCOUNTER];
-    uint64 m_uiHalion0GUID;
-    uint64 m_uiZarithrian0GUID;
-    uint64 m_uiBaltharus0GUID;
-    uint64 m_uiSaviana0GUID;
+    std::string strSaveData;
+
+    uint64 m_uiHalionGUID;
+    uint64 m_uiZarithrianGUID;
+    uint64 m_uiBaltharusGUID;
+    uint64 m_uiSavianaGUID;
 
     void Initialize()
     {
         memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
 
-        m_uiHalion0GUID		= 0;
-        m_uiZarithrian0GUID  = 0;
-        m_uiBaltharus0GUID   = 0;
-        m_uiSaviana0GUID		= 0;
+        m_uiHalionGUID             = 0;
+        m_uiZarithrianGUID         = 0;
+        m_uiBaltharusGUID          = 0;
+        m_uiSavianaGUID            = 0;
+    }
+
+    bool IsEncounterInProgress() const
+    {
+        for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+            if (m_auiEncounter[i] == IN_PROGRESS)
+                return true;
+
+        return false;
     }
 
     void OnCreatureCreate(Creature* pCreature)
     {
-        switch(pCreature->GetEntry())
+        switch (pCreature->GetEntry())
         {
-            case NPC_HALION0:
-                m_uiHalion0GUID = pCreature->GetGUID();
-                break;
-            //three dragons below set to active state once created.
-            //we must expect bigger raid to encounter main boss, and then three dragons must be active due to grid differences
-            case NPC_ZARITHRIAN0:
-                m_uiZarithrian0GUID = pCreature->GetGUID();
-                pCreature->SetActiveObjectState(true);
-                break;
-            case NPC_BALTHARUS0:
-                m_uiBaltharus0GUID = pCreature->GetGUID();
-                pCreature->SetActiveObjectState(true);
-                break;
-            case NPC_SAVIANA0:
-                m_uiSaviana0GUID = pCreature->GetGUID();
-                pCreature->SetActiveObjectState(true);
-                break;
+            case 39863: m_uiHalionGUID      = pCreature->GetGUID(); break;
+			case 39746: m_uiZarithrianGUID  = pCreature->GetGUID(); break;
+			case 39751: m_uiBaltharusGUID   = pCreature->GetGUID(); break;
+			case 39747: m_uiSavianaGUID     = pCreature->GetGUID(); break;
         }
     }
 
     void SetData(uint32 uiType, uint32 uiData)
     {
-        if (uiType == TYPE_DEAD0_EVENT)
-            m_auiEncounter[0] = uiData;
-		if (uiType == TYPE_HALION_0_EVENT)
-            m_auiEncounter[0] = uiData;
+        if (uiData == DONE)
+        {
+            OUT_SAVE_INST_DATA;
+
+            std::ostringstream saveStream;
+            saveStream << m_auiEncounter[0];
+
+            strSaveData = saveStream.str();
+
+            SaveToDB();
+            OUT_SAVE_INST_DATA_COMPLETE;
+        }
+    }
+
+    const char* Save()
+    {
+        return strSaveData.c_str();
     }
 
     uint32 GetData(uint32 uiType)
     {
-        if (uiType == TYPE_DEAD0_EVENT)
-            return m_auiEncounter[0];
-		if (uiType == TYPE_HALION_0_EVENT)
-            return m_auiEncounter[0];
-
+        switch (uiType)
+        {
+            case TYPE_HALION_EVENT:    return m_auiEncounter[0];
+        }
         return 0;
     }
 
     uint64 GetData64(uint32 uiData)
     {
-        switch(uiData)
+        switch (uiData)
         {
-            case DATA_HALION0:
-                return m_uiHalion0GUID;
-            case DATA_ZARITHRIAN0:
-                return m_uiZarithrian0GUID;
-            case DATA_BALTHARUS0:
-                return m_uiBaltharus0GUID;
-            case DATA_SAVIANA0:
-                return m_uiSaviana0GUID;
+            case DATA_HALION:              return m_uiHalionGUID;
+            case DATA_BALTHARUS:           return m_uiZarithrianGUID;
+            case DATA_ZARITHRIAN:          return m_uiBaltharusGUID;
+            case DATA_SAVIANA:             return m_uiSavianaGUID;
         }
         return 0;
+    }
+
+    void Load(const char* chrIn)
+    {
+        if (!chrIn)
+        {
+            OUT_LOAD_INST_DATA_FAIL;
+            return;
+        }
+
+        OUT_LOAD_INST_DATA(chrIn);
+
+        std::istringstream loadStream(chrIn);
+
+        loadStream >> m_auiEncounter[0] >> m_auiEncounter[1];
+
+        for(uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+            if (m_auiEncounter[i] == IN_PROGRESS)
+                m_auiEncounter[i] = NOT_STARTED;
+
+        OUT_LOAD_INST_DATA_COMPLETE;
     }
 };
 
@@ -117,6 +140,6 @@ void AddSC_instance_ruby_sanctum()
     Script *newscript;
     newscript = new Script;
     newscript->Name = "instance_ruby_sanctum";
-    newscript->GetInstanceData = GetInstanceData_instance_ruby_sanctum;
+    newscript->GetInstanceData = &GetInstanceData_instance_ruby_sanctum;
     newscript->RegisterSelf();
 }

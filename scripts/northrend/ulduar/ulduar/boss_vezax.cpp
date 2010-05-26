09b40b0edb54cac979032907e94d9330fbc5a169
GMHAZE <iceis2@hotmail.com>
2010-05-26 07:56:11 -0400

    update to vezax

--- scripts/northrend/ulduar/ulduar/boss_vezax.cpp
+++ scripts/northrend/ulduar/ulduar/boss_vezax.cpp
@@ -16,57 +16,119 @@
 
 /* ScriptData
 SDName: General Vezax
-SD%Complete: 0
+SD%Complete: 35%
 SDComment: PH.
 SDCategory: Ulduar
 EndScriptData */
 
 #include "precompiled.h"
-#include "def_ulduar.h"
+#include "ulduar.h"
 
-#define SP_AURA_OF_DESPAIR      62692
-#define SP_SHADOW_CRASH         62660
-#define H_SP_SHADOW_CRASH
-#define SP_MARK_OF_FACELESS
-#define SP_SEARING_FLAMES
-#define SP_SURGE_OF_DARKNESS
-#define SP_BERSERK              26662
-#define SP_AURA_OF_DESPAIR      62692
+// General Vezax Aura
+#define AURA_AURA_OF_DESPAIR       62692
+
+// General Vezax Spells
+#define SPELL_BERSERK              26662
+#define SPELL_MARK_OF_THE_FACELESS 63276
+#define SPELL_SARONITE_BARRIER     63364
+#define SPELL_SEARING_FLAMES       62661
+#define SPELL_SHADOW_CRASH         62660
+#define SPELL_SURGE_OF_DARKNESS    62662
+
+// General Vezax Sounds
+#define SOUND_UR_Vezax_Aggro01     15542
+#define SOUND_UR_Vezax_Attack      15550
+#define SOUND_UR_Vezax_Berserk01   15547
+#define SOUND_UR_Vezax_Death01     15546
+#define SOUND_UR_Vezax_HardMode01  15548
+#define SOUND_UR_Vezax_Kite01      15545
+#define SOUND_UR_Vezax_Slay01      15543
+#define SOUND_UR_Vezax_Slay02      15544
+#define SOUND_UR_Vezax_Wound       15549
+#define SOUND_UR_Vezax_WoundCrit   15551
+
+// General Vezax Says
+#define SAY_UR_Vezax_Aggro01  "Your destruction will herald a new age of suffering!"
+#define SAY_UR_Vezax_Slay01   "You thought to stand before the legions of death and survive?"
+#define SAY_UR_Vezax_Slay02   "Defiance... *laughs* A flaw of mortality!"
+#define SAY_UR_Vezax_Spawns   "Behold now! Terror, absolute!"
+#define SAY_UR_Vezax_Enrage01 "Your defeat was inevitable."
+#define SAY_UR_Vezax_Enrage02 "The black blood of Yogg-Saron courses through me! I...AM...UNSTOPPABLE!"
+#define SAY_UR_Vezax_Death01  "Ha-ha-ha! Oh, what horrors await..."
+
+// Saronite Vapors
+#define MOB_SAROBITE_VAPORS        33488
+
+// Saronite Vapors Spells
+#define SPELL_SAROBITE_VAPORS      63322
 
 struct MANGOS_DLL_DECL boss_vezaxAI : public ScriptedAI
 {
-    boss_vezaxAI(Creature* pCreature) : ScriptedAI(pCreature)
+    boss_generalvezaxAI(Creature* pCreature) : ScriptedAI(pCreature) 
     {
-        pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
-        Regular = pCreature->GetMap()->IsRegularDifficulty();
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        m_bIsRegularMode = pCreature->GetMap()->IsRegularDifficulty();
+
         Reset();
     }
 
-    bool Regular;
-    ScriptedInstance *pInstance;
+    ScriptedInstance* m_pInstance;
+	bool m_bIsRegularMode;
 
-    uint32 EnrageTimer;
-    /*uint32 CrashTimer;
-    uint32 MarkTimer;
-    uint32 FlamesTimer;
-    uint32 SurgeTimer;*/
+	uint32 Searing_Flames_Timer;
+	uint32 Shadow_Crash_Timer;
+	uint32 Mark_of_the_Faceless_Timer;
+	uint32 Surge_of_Darkness_Timer;
+	uint32 Saronite_Barrier_Timer;
+	uint32 Saronite_Vapors_Timer;    //Spawn 8x the entire battle
+	uint32 Saronite_Vapors_h_Timer;  //Spawn 6x the entire battle
+	uint32 Berserk_Timer;
+	bool berserk;
 
     void Reset()
     {
-        EnrageTimer = 600000; //10 minutes
-        //FlamesTimer = 10000 + rand()%10000;
-        if(pInstance) pInstance->SetData(TYPE_VEZAX, NOT_STARTED);
+		Searing_Flames_Timer = 10000;
+		Shadow_Crash_Timer = 15000;
+		Mark_of_the_Faceless_Timer = 40000;
+		Surge_of_Darkness_Timer = 30000;
+		Saronite_Barrier_Timer = 35000;
+		Saronite_Vapors_Timer = 20000;
+		Saronite_Vapors_h_Timer = 20000;
+		Berserk_Timer = 600000; //10 Minutes
+		if (m_pInstance)
+            m_pInstance->SetData(TYPE_VEZAX, NOT_STARTED);
     }
 
-    void Aggro(Unit *who) 
+    void KilledUnit(Unit *victim)
     {
-        if(pInstance) pInstance->SetData(TYPE_VEZAX, IN_PROGRESS);
-        DoCast(NULL, SP_AURA_OF_DESPAIR, true);
+		switch(urand(0, 1))
+                {
+                    case 0: 
+						DoPlaySoundToSet(m_creature, SOUND_UR_Vezax_Slay01); 
+						m_creature->MonsterYell(SAY_UR_Vezax_Slay01, LANG_UNIVERSAL, 0);
+						break;
+                    case 1:
+						DoPlaySoundToSet(m_creature, SOUND_UR_Vezax_Slay02);
+						m_creature->MonsterYell(SAY_UR_Vezax_Slay02, LANG_UNIVERSAL, 0);
+						break;
+                }
     }
 
-    void JustDied(Unit *killer)
+    void JustDied(Unit *victim)
     {
-        if(pInstance) pInstance->SetData(TYPE_VEZAX, DONE);
+		if (m_pInstance)
+            m_pInstance->SetData(TYPE_VEZAX, DONE);
+		DoPlaySoundToSet(m_creature, SOUND_UR_Vezax_Death01);
+		m_creature->MonsterYell(SAY_UR_Vezax_Death01, LANG_UNIVERSAL, 0);
+    }
+
+    void Aggro(Unit* pWho)
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_VEZAX, IN_PROGRESS);
+		DoCast(m_creature, AURA_AURA_OF_DESPAIR);
+		DoPlaySoundToSet(m_creature, SOUND_UR_Vezax_Aggro01);
+		m_creature->MonsterYell(SAY_UR_Vezax_Aggro01, LANG_UNIVERSAL, 0);
     }
 
     void UpdateAI(const uint32 diff)
@@ -74,34 +136,92 @@ struct MANGOS_DLL_DECL boss_vezaxAI : public ScriptedAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
-        /*if(FlamesTimer < diff)
+        if (Searing_Flames_Timer < diff )
         {
-            DoCast(m_creature->getVictim(), SP_SEARING_FLAMES);
-            FlamesTimer = 10000 + rand()%10000;
-        }
-        else FlamesTimer -= diff; */
+            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0))
+                DoCast(m_creature->getVictim(), SPELL_SEARING_FLAMES);
+            Searing_Flames_Timer = 25000;
+        }else Searing_Flames_Timer -= diff;
 
-        if(EnrageTimer < diff)
+		if (Shadow_Crash_Timer < diff )
         {
-            DoCast(m_creature, SP_BERSERK);
-            EnrageTimer = 30000;
-        }
-        else EnrageTimer -= diff;
+            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0))
+                DoCast(m_creature->getVictim(), SPELL_SHADOW_CRASH);
+            Shadow_Crash_Timer = 15000;
+        }else Shadow_Crash_Timer -= diff;
 
+		if (Mark_of_the_Faceless_Timer < diff )
+        {
+            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM,0))
+                DoCast(m_creature->getVictim(), SPELL_MARK_OF_THE_FACELESS);
+            Mark_of_the_Faceless_Timer = 40000;
+        }else Mark_of_the_Faceless_Timer -= diff;
+
+		if (Berserk_Timer < diff && !berserk)
+        {
+            m_creature->CastStop();
+            DoCast(m_creature, SPELL_BERSERK);
+            berserk = true;
+        }else Berserk_Timer -= diff;
+
+        DoMeleeAttackIfReady();
+
+        EnterEvadeIfOutOfCombatArea(diff);
+
+    }
+
+};
+
+CreatureAI* GetAI_boss_generalvezax(Creature* pCreature)
+{
+    return new boss_generalvezaxAI(pCreature);
+}
+
+struct MANGOS_DLL_DECL mob_saronite_vaporsrAI : public ScriptedAI
+{
+    mob_saronite_vaporsrAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        Reset();
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+    }
+
+    ScriptedInstance* m_pInstance;
+    bool m_bIsRegularMode;
+
+    void Reset()
+    {
+    }
+
+	void JustDied(Unit *victim)
+    {
+		DoCast(m_creature, SPELL_SAROBITE_VAPORS);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
         DoMeleeAttackIfReady();
     }
 };
 
-CreatureAI* GetAI_boss_vezax(Creature* pCreature)
+CreatureAI* GetAI_mob_saronite_vaporsr(Creature* pCreature)
 {
-    return new boss_vezaxAI(pCreature);
+    return new mob_saronite_vaporsrAI(pCreature);
 }
 
 void AddSC_boss_vezax()
 {
     Script *newscript;
+
     newscript = new Script;
     newscript->Name = "boss_vezax";
-    newscript->GetAI = &GetAI_boss_vezax;
+    newscript->GetAI = &GetAI_boss_generalvezax;
     newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "mob_saronite_vaporsr";
+    newscript->GetAI = &GetAI_mob_saronite_vaporsr;
+    newscript->RegisterSelf();
+
 }
+/*UPDATE `creature_template` SET `ScriptName`='boss_vezax' WHERE (`entry`='33271') LIMIT 1
+  UPDATE `creature_template` SET `ScriptName`='mob_saronite_vaporsr' WHERE (`entry`='33488') LIMIT 1*/

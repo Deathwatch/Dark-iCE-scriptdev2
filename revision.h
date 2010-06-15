062c7e4c56775a731995af33099b50e13aa2263c
GMHAZE <iceis2@hotmail.com>
2010-06-15 16:02:30 -0400

    reset

--- revision.h
+++ revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "3d6711e952eaf6cbe03e44ba63bbf1fc6f846f38"
+ #define REVISION_ID "*"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__

b4cf126e4c8bf547c1ab68b9afc728e69f43e11f
GMHAZE <iceis2@hotmail.com>
2010-06-15 15:59:05 -0400

    readme rewrite step1

--- revision.h
+++ revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "3836847411ef18903d6f32971d26320fe23daead"
+ #define REVISION_ID "3d6711e952eaf6cbe03e44ba63bbf1fc6f846f38"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__

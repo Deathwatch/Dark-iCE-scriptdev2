65f77a11481fe0c71b3aab5f9b7a9291838ba082
GMHAZE <iceis2@hotmail.com>
2010-07-01 19:51:11 -0400

    boss broggok - intro event and soft reset

--- revision.h
+++ revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "338a027a3076883c06b740fe3b6d7dfe9a6f6341"
- #define REVISION_DATE "2010-06-24"
- #define REVISION_TIME "16:57:04"
+ #define REVISION_ID "*"
+ #define REVISION_DATE "*"
+ #define REVISION_TIME "*"
 #endif // __REVISION_H__
